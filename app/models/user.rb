20ade287b8ef8a010c9a8abaab67792a34298634
Jessica Claire Edwards <jessica@eml.cc>
2015-07-10 21:18:30 +1000

    Validate email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, 
+    uniqueness: true,
+    format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9\.-]+\.[A-Za-z]+\Z/ }
 end
