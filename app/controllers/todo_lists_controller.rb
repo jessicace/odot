1ac85f541ee3b53ffd41c1271d3b751008984ee6
Jessica Claire Edwards <jessica@eml.cc>
2015-07-13 19:43:50 +1000

    Updated todo lists controller

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,31 +1,21 @@
 class TodoListsController < ApplicationController
-  before_action :set_todo_list, only: [:show, :edit, :update, :destroy]
+  before_action :require_user
+  before_action :find_todo_list, only: [:show, :edit, :update, :destroy]
 
-  # GET /todo_lists
-  # GET /todo_lists.json
   def index
-    @todo_lists = TodoList.all
+    @todo_lists = current_user.todo_lists
   end
 
-  # GET /todo_lists/1
-  # GET /todo_lists/1.json
-  def show
-  end
+  def show; end
 
-  # GET /todo_lists/new
   def new
-    @todo_list = TodoList.new
+    @todo_list = current_user.todo_lists.new
   end
 
-  # GET /todo_lists/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /todo_lists
-  # POST /todo_lists.json
   def create
-    @todo_list = TodoList.new(todo_list_params)
-
+    @todo_list = current_user.todo_lists.new(todo_list_params)
     respond_to do |format|
       if @todo_list.save
         format.html { redirect_to @todo_list, notice: 'Todo list was successfully created.' }
@@ -37,8 +27,6 @@ class TodoListsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /todo_lists/1
-  # PATCH/PUT /todo_lists/1.json
   def update
     respond_to do |format|
       if @todo_list.update(todo_list_params)
@@ -51,8 +39,6 @@ class TodoListsController < ApplicationController
     end
   end
 
-  # DELETE /todo_lists/1
-  # DELETE /todo_lists/1.json
   def destroy
     @todo_list.destroy
     respond_to do |format|
@@ -62,12 +48,10 @@ class TodoListsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_todo_list
+    def find_todo_list
       @todo_list = TodoList.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def todo_list_params
       params.require(:todo_list).permit(:title, :description)
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
   # GET /users.json
@@ -62,13 +62,11 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+  def find_user
+    @user = User.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
-    end
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
+  end
 end
