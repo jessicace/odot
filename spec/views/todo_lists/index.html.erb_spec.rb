430a97edc0c0357c89ac230b1aee2d127f7d92c2
Jessica Claire Edwards <jessica@eml.cc>
2015-07-21 12:32:32 +1000

    Added todo items feature tests

--- spec/views/todo_lists/index.html.erb_spec.rb
+++ spec/views/todo_lists/index.html.erb_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "todo_lists/index", type: :view do
+
   before(:each) do
     assign(:todo_lists, [ TodoList.create!(title: "Title"),
                          TodoList.create!(title: "Title") ])
@@ -8,6 +9,7 @@ RSpec.describe "todo_lists/index", type: :view do
 
   it "renders a list of todo_lists" do
     render
-    assert_select "ul>li", text: "Title".to_s, count: 2
+    assert_select "ul.todo_lists li div", text: "Title".to_s, count: 2
   end
+  
 end
